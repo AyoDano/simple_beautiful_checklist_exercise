0821f7b2293f38fb0d0aaf8c8bcaa15a99208361
AyoDano <100164168+AyoDano@users.noreply.github.com>
2025-03-04 16:11:11 +0100

    feat: mock database content for testing sharedPreferenceRepository integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:simple_beautiful_checklist_exercise/shared/database_repository.dart';
 import 'package:simple_beautiful_checklist_exercise/shared/mock_database.dart';
 
@@ -15,76 +14,6 @@ void main() async {
   final DatabaseRepository repository = SharedPreferencesRepository();
 
   runApp(MainApp(repository: repository));
-}
-
-class SharedPreferencesRepository implements DatabaseRepository {
-  static const String _key = "tasks";
-
-// Überschreiben der bestehenden Methoden
-  @override
-// Hinzufügefunktion
-  Future<void> addItem(String item) async {
-// Zugriff auf SharedPref-Instanz
-    final prefs = await SharedPreferences.getInstance();
-// Auslesen der bestehenden Liste
-    final tasks = prefs.getStringList(_key) ?? [];
-//Hinzufügen von einer neuen Task
-    tasks.add(item);
-// Liste neu speichern
-    await prefs.setStringList(_key, tasks);
-  }
-
-  @override
-//Löschfunktion
-  Future<void> deleteItem(int index) async {
-// Zugriff auf SharedPref-Instanz
-    final prefs = await SharedPreferences.getInstance();
-// Auslesen der bestehenden Liste
-    final tasks = prefs.getStringList(_key) ?? [];
-// Löschen der Task
-    tasks.removeAt(index);
-// Liste neu speichern/aktualisieren
-    await prefs.setStringList(_key, tasks);
-  }
-
-  @override
-//Bearbeitefunktion
-  Future<void> editItem(int index, String newItem) async {
-// Zugriff auf SharedPref-Instanz
-    final prefs = await SharedPreferences.getInstance();
-// Auslesen der bestehenden Liste
-    final tasks = prefs.getStringList(_key) ?? [];
-// Bearbeiten der Task
-    if (index >= 0 && index < tasks.length) {
-      tasks[index] = newItem;
-// Liste neu Speichern / aktualisieren
-      await prefs.setStringList(_key, tasks);
-    } else {
-      print("Falscher Index ($index)");
-    }
-  }
-
-  @override
-  //Abfragefunktion
-  Future<List<String>> getItems() async {
-// Zugriff auf SharedPref-Instanz
-    final prefs = await SharedPreferences.getInstance();
-// Auslesen der bestehenden Liste
-    final tasks = prefs.getStringList(_key) ?? [];
-// Zurückgeben (ausgeben) der aktuellen Liste
-    return tasks;
-  }
-
-  @override
-// Anzahlfunktion um gesamtzahl zu ermitteln
-  Future<int> get itemCount async {
-    // Zugriff auf SharedPref-Instanz
-    final prefs = await SharedPreferences.getInstance();
-// Auslesen der bestehenden Liste
-    final tasks = prefs.getStringList(_key) ?? [];
-// Alle inhalte der Liste werden zurückgegeben
-    return tasks.length;
-  }
 }
 
 class MainApp extends StatelessWidget {
--- lib/shared/mock_database.dart
+++ lib/shared/mock_database.dart
@@ -1,36 +1,72 @@
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:simple_beautiful_checklist_exercise/shared/database_repository.dart';
 
-class MockDatabase implements DatabaseRepository {
-  final List<String> _items = [];
+class SharedPreferencesRepository implements DatabaseRepository {
+  static const String _key = "tasks";
 
+// Überschreiben der bestehenden Methoden
   @override
-  Future<int> get itemCount async {
-    await Future.delayed(const Duration(milliseconds: 100));
-    return _items.length;
-  }
-
-  @override
-  Future<List<String>> getItems() async {
-    await Future.delayed(const Duration(milliseconds: 100));
-    return _items;
-  }
-
-  @override
+// Hinzufügefunktion
   Future<void> addItem(String item) async {
-    //make sure item doesn't exist yet and is not empty
-    if (item.isNotEmpty && !_items.contains(item)) _items.add(item);
+// Zugriff auf SharedPref-Instanz
+    final prefs = await SharedPreferences.getInstance();
+// Auslesen der bestehenden Liste
+    final tasks = prefs.getStringList(_key) ?? [];
+//Hinzufügen von einer neuen Task
+    tasks.add(item);
+// Liste neu speichern
+    await prefs.setStringList(_key, tasks);
   }
 
   @override
+//Löschfunktion
   Future<void> deleteItem(int index) async {
-    _items.removeAt(index);
+// Zugriff auf SharedPref-Instanz
+    final prefs = await SharedPreferences.getInstance();
+// Auslesen der bestehenden Liste
+    final tasks = prefs.getStringList(_key) ?? [];
+// Löschen der Task
+    tasks.removeAt(index);
+// Liste neu speichern/aktualisieren
+    await prefs.setStringList(_key, tasks);
   }
 
   @override
+//Bearbeitefunktion
   Future<void> editItem(int index, String newItem) async {
-    // make sure not empty and not same as other
-    if (newItem.isNotEmpty && !_items.contains(newItem)) {
-      _items[index] = newItem;
+// Zugriff auf SharedPref-Instanz
+    final prefs = await SharedPreferences.getInstance();
+// Auslesen der bestehenden Liste
+    final tasks = prefs.getStringList(_key) ?? [];
+// Bearbeiten der Task
+    if (index >= 0 && index < tasks.length) {
+      tasks[index] = newItem;
+// Liste neu Speichern / aktualisieren
+      await prefs.setStringList(_key, tasks);
+    } else {
+      print("Falscher Index ($index)");
     }
+  }
+
+  @override
+  //Abfragefunktion
+  Future<List<String>> getItems() async {
+// Zugriff auf SharedPref-Instanz
+    final prefs = await SharedPreferences.getInstance();
+// Auslesen der bestehenden Liste
+    final tasks = prefs.getStringList(_key) ?? [];
+// Zurückgeben (ausgeben) der aktuellen Liste
+    return tasks;
+  }
+
+  @override
+// Anzahlfunktion um gesamtzahl zu ermitteln
+  Future<int> get itemCount async {
+    // Zugriff auf SharedPref-Instanz
+    final prefs = await SharedPreferences.getInstance();
+// Auslesen der bestehenden Liste
+    final tasks = prefs.getStringList(_key) ?? [];
+// Alle inhalte der Liste werden zurückgegeben
+    return tasks.length;
   }
 }
