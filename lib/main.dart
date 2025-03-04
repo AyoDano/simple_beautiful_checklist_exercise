4528e842fc96e6739dcd675bd74c1a200a5789f9
AyoDano <100164168+AyoDano@users.noreply.github.com>
2025-03-04 15:18:22 +0100

    feat: add addItem method for item creation functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:simple_beautiful_checklist_exercise/shared/database_repository.dart';
 import 'package:simple_beautiful_checklist_exercise/shared/mock_database.dart';
 
@@ -10,10 +11,49 @@ void main() async {
   // Wird benötigt, um auf SharedPreferences zuzugreifen
   WidgetsFlutterBinding.ensureInitialized();
 
-  // TODO: Hier statt MockDatabase() ein SharedPreferencesRepository() verwenden.
-  final DatabaseRepository repository = MockDatabase();
+// Hinzugefügt SharedPferenceRepository
+  final DatabaseRepository repository = SharedPreferencesRepository();
 
   runApp(MainApp(repository: repository));
+}
+
+class SharedPreferencesRepository implements DatabaseRepository {
+  static const String _key = "tasks";
+
+// Überschreiben der bestehenden Methoden
+  @override
+  Future<void> addItem(String item) async {
+// Zugriff auf SharedPref-Instanz
+    final prefs = await SharedPreferences.getInstance();
+// Auslesen der bestehenden Liste
+    final tasks = prefs.getStringList(_key) ?? [];
+//Hinzufügen von einer neuen Task
+    tasks.add(item);
+// Liste neu speichern
+    await prefs.setStringList(_key, tasks);
+  }
+
+  @override
+  Future<void> deleteItem(int index) {
+    // TODO: implement deleteItem
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> editItem(int index, String newItem) {
+    // TODO: implement editItem
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<String>> getItems() {
+    // TODO: implement getItems
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement itemCount
+  Future<int> get itemCount => throw UnimplementedError();
 }
 
 class MainApp extends StatelessWidget {
